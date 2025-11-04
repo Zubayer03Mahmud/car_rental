4bc88f1bb86593b627301e9f7507ca404003ee2c
Zubayer03Mahmud <zubayer.mahmud03@gmail.com>
2025-11-04 13:19:04 +0600

    owner dashboard setup

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -6,6 +6,16 @@ import CarDetails from './pages/CarDetails';
 import Cars from './pages/Cars';
 import MyBookings from './pages/MyBookings';
 import Footer from './components/Footer';
+import Layout from './pages/Owner/Layout';
+import Dashboard from './pages/Owner/DAshboard';
+import AddCar from './pages/Owner/AddCar';
+import ManageCars from './pages/Owner/ManageCars';
+import ManageBookings from './pages/Owner/ManageBookings';
+
+
+
+
+
 
 const App = () => {
   const [showLogin, setShowLogin] = useState(false);
@@ -20,6 +30,15 @@ const App = () => {
         <Route path="/car-details/:id" element={<CarDetails />} />
         <Route path="/cars" element={<Cars />} />
         <Route path="/my-bookings" element={<MyBookings />} />
+
+        <Route path='/owner' element={ <Layout /> }> 
+          <Route index element={ <Dashboard />} />
+          <Route path='add-car' element={ <AddCar />} />
+          <Route path='manage-cars' element={ <ManageCars />} />
+          <Route path='manage-booking' element={<ManageBookings />} />    
+
+        </Route>
+
       </Routes>
 
       {!isOwnerPath && <Footer />}
---
+++ client/src/pages/Owner/DAshboard.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const DAshboard = () => {
+  return (
+    <div>
+      
+    </div>
+  )
+}
+
+export default DAshboard
