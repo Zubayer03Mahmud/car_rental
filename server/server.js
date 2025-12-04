3b5dc15dc6f76b29ca58ee6e91d7b42d8a11769a
Zubayer03Mahmud <zubayer.mahmud03@gmail.com>
2025-12-04 19:30:56 +0600

    connecting db with mongodb

--- server/server.js
+++ server/server.js
@@ -1,10 +1,14 @@
 import express from "express";
 import "dotenv/config";
 import cors from "cors";
+import connectDB from "./configs/db.js";
 
 //initialize express app
 
 const app = express()
+
+//connect database
+await connectDB();
 
 //middleware
 
