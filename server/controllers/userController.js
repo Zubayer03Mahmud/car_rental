a4857a6c99b2afb5c46421358776cc221052867d
Zubayer03Mahmud <zubayer.mahmud03@gmail.com>
2025-12-06 18:18:15 +0600

    adding authorization

--- server/controllers/userController.js
+++ server/controllers/userController.js
@@ -53,5 +53,15 @@ export const loginUser = async (req, res) => {
   }
 };
 
-
 //get user data using Token(JWT)
+
+export const getUserData = async (req, res) => {
+  try{
+    const {user} = req;
+    res.json({success: true, user})
+ 
+  }catch{
+    console.log(error.message);
+    res.json({success: false, message: error.message})
+  }
+}
